4af277c116e91ca10dae510b6c62bd1ba497e68c
Aiperi Faisova <alice.faisova@gmail.com>
2021-06-28 01:18:27 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String name = "Erlan";
+	int age = 19;
+	int temp = 27;
+	if (age <=45 & age > 20 & temp < 30 & temp > -20){
+        System.out.println(name + " может сегодня гулять");
+    }
+	else if(age < 20 & temp > 0 & temp < 28){
+        System.out.println(name + " может сегодня гулять");
+    }
+	else if (age > 45 & age > -10 & temp < 25){
+        System.out.println(name + "может сегодня гулять");
+    }
+	else {
+        System.out.println("сегодня гулять нельзя");
+    }
+    }
+}
