85eb35b896aa33d114184d0588cf084497c29e56
erlankanybekov <86570752+erlankanybekov@users.noreply.github.com>
2021-06-28 17:25:21 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String name = "Erlan";
-	int age = 19;
-	int temp = 27;
-	if (age <=45 & age > 20 & temp < 30 & temp > -20){
-        System.out.println(name + " может сегодня гулять");
+        String name = "Erlan";
+        int age = 19;
+        boolean isSunny = true;
+        int temp = 27;
+        if (isSunny && temp < 30 & age > 20 & age < 45 & temp > -20  ){
+            System.out.println(name + " может сегодня гулять");
+        }
+        else if (isSunny && temp > 0 & temp < 28 & age < 20  ){
+            System.out.println(name +" может сегодня гулять");
+        }
+        else if (isSunny && temp > -10 & age > 45 & temp < 25){
+            System.out.println(name + " может сегодня гулять");
+        }
+        else {
+            System.out.println("гулять сегодня нельзя"); }
     }
-	else if(age < 20 & temp > 0 & temp < 28){
-        System.out.println(name + " может сегодня гулять");
-    }
-	else if (age > 45 & temp > -10 & temp < 25){
-        System.out.println(name + "может сегодня гулять");
-    }
-	else {
-        System.out.println("сегодня гулять нельзя");
-    }
-    }
+
+
 }
